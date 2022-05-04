14f9183db6079f2ec0726359ae2a8e9618d4b6b6
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-05-04 15:34:53 +0100

    rook logic

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,23 @@
 import Piece from './piece';
-
+import Square from '../square';
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let array = []
+
+        for (let i = 0; i <= 7; i++) {
+            array.push(Square.at(location.row, i))
+        }
+
+        for (let i = 0; i <= 7; i++) {
+            array.push(Square.at(i, location.col))
+        }
+
+        let filteredArray = array.filter(square => square.row !== location.row || square.col !== location.col);
+        return filteredArray;
     }
 }
