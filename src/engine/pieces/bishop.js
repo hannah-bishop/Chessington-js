f37c6a5c2b3ee2ba0e797142189f75daaefa3bb9
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-05-04 16:02:53 +0100

    bishop logic

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,27 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this)
+        let array = []
+
+        for (let i= 1 ; location.row + i <= 7 && location.col + i <= 7; i++){
+            array.push(Square.at(location.row + i, location.col + i));
+        }
+
+        for (let i= 1 ; location.row - i >= 0 && location.col - i >= 0; i++){
+            array.push(Square.at(location.row - i, location.col - i));
+        }
+
+        for (let i= 1 ; location.row + i <=7 && location.col - i >= 0; i++){
+            array.push(Square.at(location.row + i, location.col - i));
+        }
+
+        for (let i= 1 ; location.col + i <=7 && location.row - i >= 0; i++){
+            array.push(Square.at(location.row - i,location.col + i));
+        }
+
+
+        console.log(array);
+        return array;
     }
 }
