bb63b558cf92b0d621d71c10b16493cd0814a694
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-05-05 15:45:57 +0100

    pawn end of board

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,29 +11,35 @@ export default class Pawn extends Piece {
         let array =[]
         let location = board.findPiece(this)
         if (this.player === Player.WHITE) {
+            if (location.row === 7) {
+                console.log(array);
+                return array;
+            }
             let hasPieceInFront = board.getPiece(Square.at(location.row + 1, location.col));
             let hasPieceTwoInFront = board.getPiece(Square.at(location.row + 2, location.col));
-            if (location.row > 1 && !hasPieceInFront) {
-                array.push(Square.at(location.row + 1, location.col))
-            } else if (location.row === 1 && !hasPieceInFront){
-                array.push(Square.at(location.row + 1, location.col))
-                if (!hasPieceTwoInFront){
+
+            if (!hasPieceInFront) {
+                array.push(Square.at(location.row + 1, location.col));
+                if (location.row === 1 && !hasPieceTwoInFront) {
                     array.push(Square.at(location.row + 2, location.col));
                 }
             }
         } else {
+            if (location.row === 0) {
+                console.log(array);
+                return array;
+            }
             let hasPieceInFront = board.getPiece(Square.at(location.row - 1, location.col));
             let hasPieceTwoInFront = board.getPiece(Square.at(location.row - 2, location.col));
-            if (location.row < 6 && !hasPieceInFront){
-                array.push(Square.at(location.row - 1, location.col))
-            } else if (location.row === 6 && !hasPieceInFront) {
-                array.push(Square.at(location.row - 1, location.col));
-                if (!hasPieceTwoInFront){
-                    array.push(Square.at(location.row - 2, location.col));
-                }
-            }
-        }
+                if (!hasPieceInFront) {
+                    array.push(Square.at(location.row - 1, location.col)); 
+                    if (location.row === 6 && !hasPieceTwoInFront) {
+                        array.push(Square.at(location.row - 2, location.col));
         
+                    }
+                } 
+            
+        } //console.log(array) 
         return array
     }
 }
